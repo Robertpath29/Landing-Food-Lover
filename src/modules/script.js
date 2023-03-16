403042de1a3d59dee0a1b41c212566e7a905ca82
Yevhenii <Robertpath29@gmail.com>
2023-03-16 12:54:04 +0200

    fix error slow load from mobile device

--- src/modules/script.js
+++ src/modules/script.js
@@ -1,7 +1,7 @@
 import { animationImg } from "./animImg.js";
 import { navigation } from "./nav.js";
 import { moveToElem } from "./nav.js"
-
+loadHeroBanner();
 
 $(window).on("load", function () {
     let scrollPages = Math.floor($(document).scrollTop());
@@ -17,3 +17,10 @@ $(window).on("load", function () {
     $(".header").on("click", moveToElem);
 
 });
+
+function loadHeroBanner() {
+    if ($(window).width() > 856) {
+        $(".hero-banner").html(`<video src="./video/heroBanner.mp4" width="100%" autoplay playsinline loop muted></video>`);
+    }
+
+}
